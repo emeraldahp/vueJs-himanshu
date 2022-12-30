deb5c2d96956252580d74630c56621c8c6d710d6
Himanshu Patel <himanshu.patel.main@gmail.com>
2022-12-30 21:37:36 +0530

    TestingDev component

--- vue-firstproject/src/App.vue
+++ vue-firstproject/src/App.vue
@@ -13,7 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>      
+        <RouterLink to="/about">About</RouterLink> 
+        <RouterLink to="/test">Test</RouterLink>    
       </nav>
     </div>
   </header>
---
+++ vue-firstproject/src/components/TestingDev.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Name: Himanshu Patel</h1>
+        <h1>BTECH</h1>
+    </div>
+</template>
